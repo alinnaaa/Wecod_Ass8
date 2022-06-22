709bd706b6550aaa4069b08c2f222316591fc9bf
Alina Sdqi <106480594+alinnaaa@users.noreply.github.com>
2022-06-22 17:16:17 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,9 +19,14 @@ class HomeWork extends StatefulWidget {
 }
 
 class HomeWorkState extends State<HomeWork> {
-  bool isVisible = false;
-  bool isVisible2 = false;
-  bool isVisible3 = false;
+  String img1 =
+      'https://images.pexels.com/photos/302743/pexels-photo-302743.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500';
+  String img2 =
+      'https://media.istockphoto.com/photos/taj-mahal-mausoleum-in-agra-picture-id1146517111?k=20&m=1146517111&s=612x612&w=0&h=vHWfu6TE0R5rG6DJkV42Jxr49aEsLN0ML-ihvtim8kk=';
+  String img3 =
+      'https://images.pexels.com/photos/1172253/pexels-photo-1172253.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500';
+  String choose =
+      'https://images.pexels.com/photos/302743/pexels-photo-302743.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500';
 
   @override
   Widget build(BuildContext context) {
@@ -30,63 +37,16 @@ class HomeWorkState extends State<HomeWork> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            /*   Container(
+            Container(
+              color: Colors.black54,
               padding: EdgeInsets.fromLTRB(50, 25, 50, 25),
+              width: MediaQuery.of(context).size.width,
+              height: 150,
               child: Image.network(
-                "https://media.istockphoto.com/photos/mountain-landscape-picture-id517188688?k=20&m=517188688&s=612x612&w=0&h=i38qBm2P-6V4vZVEaMy_TaTEaoCMkYhvLCysE7yJQ5Q=",
-                height: 300,
-                width: 300,
+                choose,
+                height: 150,
+                width: MediaQuery.of(context).size.width,
               ),
-            ),
-*/
-
-            Visibility(
-              visible: isVisible,
-              maintainSize: true,
-              maintainAnimation: true,
-              maintainState: true,
-              //child: Container(
-              //padding: EdgeInsets.fromLTRB(50, 25, 50, 25),
-              child: Image.network(
-                "https://images.pexels.com/photos/302743/pexels-photo-302743.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-                height: 130,
-                width: 130,
-              ),
-              //),
-            ),
-
-//****************************************************************************** */
-
-            Visibility(
-              visible: isVisible2,
-              maintainSize: true,
-              maintainAnimation: true,
-              maintainState: true,
-              //child: Container(
-              //padding: EdgeInsets.fromLTRB(50, 25, 50, 25),
-              child: Image.network(
-                "https://media.istockphoto.com/photos/taj-mahal-mausoleum-in-agra-picture-id1146517111?k=20&m=1146517111&s=612x612&w=0&h=vHWfu6TE0R5rG6DJkV42Jxr49aEsLN0ML-ihvtim8kk=",
-                height: 130,
-                width: 130,
-              ),
-              //),
-            ),
-
-//****************************************************************************** */
-
-            Visibility(
-              visible: isVisible3,
-              maintainSize: true,
-              maintainAnimation: true,
-              maintainState: true,
-              //child: Container(
-              //padding: EdgeInsets.fromLTRB(50, 25, 50, 25),
-              child: Image.network(
-                "https://images.pexels.com/photos/1172253/pexels-photo-1172253.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-                height: 130,
-                width: 130,
-              ),
-              //),
             ),
 
 //****************************************************************************** */
@@ -96,7 +56,7 @@ class HomeWorkState extends State<HomeWork> {
 //****************************************************************************** */
                 Container(
                   child: IconButton(
-                    padding: EdgeInsets.fromLTRB(2.5, 5, 0, 50),
+                    padding: EdgeInsets.fromLTRB(5, 5, 0, 50),
                     icon: Image.network(
                       "https://images.pexels.com/photos/302743/pexels-photo-302743.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
                       height: 130,
@@ -105,7 +65,7 @@ class HomeWorkState extends State<HomeWork> {
                     iconSize: 130,
                     onPressed: () {
                       setState(() {
-                        isVisible = !isVisible;
+                        choose = img1;
                       });
                     },
                   ),
@@ -115,16 +75,16 @@ class HomeWorkState extends State<HomeWork> {
 
                 Container(
                   child: IconButton(
-                    padding: EdgeInsets.fromLTRB(2.5, 5, 0, 50),
+                    padding: EdgeInsets.fromLTRB(5, 5, 0, 50),
                     icon: Image.network(
-                      "https://media.istockphoto.com/photos/taj-mahal-mausoleum-in-agra-picture-id1146517111?k=20&m=1146517111&s=612x612&w=0&h=vHWfu6TE0R5rG6DJkV42Jxr49aEsLN0ML-ihvtim8kk=",
+                      'https://media.istockphoto.com/photos/taj-mahal-mausoleum-in-agra-picture-id1146517111?k=20&m=1146517111&s=612x612&w=0&h=vHWfu6TE0R5rG6DJkV42Jxr49aEsLN0ML-ihvtim8kk=',
                       height: 130,
                       width: 130,
                     ),
                     iconSize: 130,
                     onPressed: () {
                       setState(() {
-                        isVisible2 = !isVisible2;
+                        choose = img2;
                       });
                     },
                   ),
@@ -133,7 +93,7 @@ class HomeWorkState extends State<HomeWork> {
 
                 Container(
                   child: IconButton(
-                    padding: EdgeInsets.fromLTRB(2.5, 5, 0, 50),
+                    padding: EdgeInsets.fromLTRB(5, 5, 0, 50),
                     icon: Image.network(
                       "https://images.pexels.com/photos/1172253/pexels-photo-1172253.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
                       height: 130,
@@ -142,7 +102,7 @@ class HomeWorkState extends State<HomeWork> {
                     iconSize: 130,
                     onPressed: () {
                       setState(() {
-                        isVisible3 = !isVisible3;
+                        choose = img3;
                       });
                     },
                   ),
@@ -156,13 +116,3 @@ class HomeWorkState extends State<HomeWork> {
     );
   }
 }
-
-
-
-/*
-              child: Image.network(
-                "https://media.istockphoto.com/photos/mountain-landscape-picture-id517188688?k=20&m=517188688&s=612x612&w=0&h=i38qBm2P-6V4vZVEaMy_TaTEaoCMkYhvLCysE7yJQ5Q=",
-                height: 200,
-                width: 200,
-              ),
-              */
